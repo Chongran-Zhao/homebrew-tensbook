5b48b6e08a2d0f718ebac6cbb215decab0ed2c1d
Chongran-Zhao <imchongran@gmail.com>
2026-07-05 17:21:57 +0800

    tensbook 1.1.1

--- Casks/tensbook.rb
+++ Casks/tensbook.rb
@@ -1,8 +1,8 @@
 cask "tensbook" do
-  version "1.1.0"
-  sha256 "aad9155d0dddaea568c22fcf2be50bcaf521a642184159c2b5e9c7aa09c7ed21"
+  version "1.1.1"
+  sha256 "70b9349627b5fc31ce043fe2f7d07eae0618b11a8618135e787eed3a619bb6a3"
 
-  url "https://github.com/Chongran-Zhao/Tensbook/releases/download/v1.1.0/Tensbook-v#{version}-aarch64-apple-darwin.dmg"
+  url "https://github.com/Chongran-Zhao/Tensbook/releases/download/v1.1.1/Tensbook-v#{version}-aarch64-apple-darwin.dmg"
   name "Tensbook"
   desc "Symbolic math notebook for tensors, calculus, and ODEs"
   homepage "https://github.com/Chongran-Zhao/Tensbook"
